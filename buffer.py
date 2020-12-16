Add reward scaling, fully connected layers to convnet, set fixed lr, 100k replay buffer, train longer, scoring, plotting, persist and load model, evaluation runner

buffer.py
@@ -4,7 +4,7 @@ from collections import deque
 
 class ReplayBuffer():
     def __init__(self, batch_size):
-        self.replay_buffer = deque(maxlen=10000)
+        self.replay_buffer = deque(maxlen=100000)
         self.batch_size = batch_size
         self.device = torch.device("cpu")
 
@@ -16,7 +16,7 @@ class ReplayBuffer():
         rewards = torch.tensor([s[2] for s in samples]).float().unsqueeze(1).to(self.device)
         next_states = torch.stack([s[3] for s in samples])
         dones = torch.tensor([s[4] for s in samples]).float().unsqueeze(1).to(self.device)
-        print(states.shape, actions.shape, rewards.shape, next_states.shape, dones.shape)
+        # print(states.shape, actions.shape, rewards.shape, next_states.shape, dones.shape)
         return states, actions, rewards, next_states, dones
     
     def add(self, sars):

neuralnetwork.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Normal
 
-convoutputsize = 8192
+convnetworkoutputsize = 256
+convoutputsize = 8192 
 
 class ConvNetwork(nn.Module):
     def __init__(self):    
@@ -12,6 +13,10 @@ class ConvNetwork(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, 3)
         self.conv3 = nn.Conv2d(64, 128, 3)
         #self.conv4 = nn.Conv2d(128, 128, 3)
+        #self.conv5 = nn.Conv2d(128, 128, 3)
+        self.fc1 = nn.Linear(convoutputsize, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, convnetworkoutputsize)
         self.pool = nn.MaxPool2d(2, 2)
 
     def forward(self, state):
@@ -20,12 +25,15 @@ class ConvNetwork(nn.Module):
         x = self.pool(F.relu(self.conv3(x)))
         #x = self.conv4(x)
         x = x.view(x.shape[0], -1) #refit x
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 class StateValueNetwork(nn.Module):
     def __init__(self):    
         super().__init__()#NeuralNetwork, self
-        self.fc1 = nn.Linear(convoutputsize, 128)
+        self.fc1 = nn.Linear(convnetworkoutputsize, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, 1)        
 
@@ -38,13 +46,12 @@ class StateValueNetwork(nn.Module):
 class ActionValueNetwork(nn.Module):
     def __init__(self):    
         super().__init__()#NeuralNetwork, self
-        self.fc1 = nn.Linear(convoutputsize+11, 128)
+        self.fc1 = nn.Linear(convnetworkoutputsize+11, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, 1)
         # uniform init layer 3
 
     def forward(self, state, action):
-        print(action.shape)
         x = F.relu(self.fc1(torch.cat((state, action), dim=1)))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -53,7 +60,7 @@ class ActionValueNetwork(nn.Module):
 class PolicyNetwork(nn.Module):
     def __init__(self):    
         super().__init__()#NeuralNetwork, self
-        self.fc1 = nn.Linear(convoutputsize, 128)
+        self.fc1 = nn.Linear(convnetworkoutputsize, 128)
         self.fc2 = nn.Linear(128, 64)
 
         self.mean_fc = nn.Linear(64, 11)

runner.py
@@ -1,10 +1,13 @@
+import torch
 from obstacle_tower_env import ObstacleTowerEnv
 from matplotlib import pyplot as plt
 from collections import deque
 from sac import SoftActorCriticAgent
 import torchvision.transforms.functional as TF
+import numpy as np
+import matplotlib.pyplot as plt
 
-env = ObstacleTowerEnv(retro=False, realtime_mode=True)
+env = ObstacleTowerEnv(retro=False, realtime_mode=False)
 print(env.action_space)
 print(env.observation_space)
 
@@ -15,15 +18,74 @@ state = env.reset()
 state = state[0]
 state = TF.to_tensor(state)
 print(state.size)
-#for episode in range(10):
-for steps in range(1000):
-    actions, actions_env_format = agent.select_actions(state)
-    print('rnr21', actions.shape, actions_env_format.shape)
-    next_state, reward, done, info = env.step(actions_env_format)
-    next_state = next_state[0]
-    next_state = TF.to_tensor(next_state)
-    agent.replay_buffer.add((state, actions, reward, next_state, done))    
-    agent.train()
-    state = next_state
-    if(done):
-        break
+scores = []
+mean_scores_100 = deque(maxlen=100)
+version = 'v3'
+for episode in range(400):
+    timesteps = 0
+    rewards = 0
+    for steps in range(10000):
+        timesteps += 1
+        actions, actions_env_format = agent.select_actions(state)
+        next_state, reward, done, info = env.step(actions_env_format)
+        next_state = next_state[0]
+        next_state = TF.to_tensor(next_state)
+        agent.replay_buffer.add((state, actions, reward, next_state, done))    
+        agent.train()
+        state = next_state
+        rewards += reward
+        if(done):
+            break
+    scores.append(rewards)
+    mean_scores_100.append(rewards)
+    print('episode {} frames {} rewards {:.2f} mean score {:.2f}'.format(episode, timesteps, rewards, np.mean(mean_scores_100)))
+    if(episode % 100 == 0):
+        torch.save(agent.conv_net.state_dict(), 'checkpoints/conv_net_checkpoint_{}.pth'.format(version))
+        torch.save(agent.critic_v.state_dict(), 'checkpoints/critic_v_checkpoint_{}.pth'.format(version))
+        torch.save(agent.critic_q_1.state_dict(), 'checkpoints/critic_q_1_checkpoint_{}.pth'.format(version))
+        torch.save(agent.critic_q_2.state_dict(), 'checkpoints/critic_q_2_checkpoint_{}.pth'.format(version))
+        torch.save(agent.actor.state_dict(), 'checkpoints/actor_checkpoint_{}.pth'.format(version))
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores)), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.savefig('results/{}_scores.png'.format(version))
+
+# todos:
+# run on openai gym Mountaincar or cartpole to test implementation
+# 2 hidden layers, 256 units
+# build testrunner.py
+# up batch size, paper 256
+# up buffer 10^6
+# timer
+# tweak tests reward scale 5, 20
+# set up pc3?
+# reduce size of convnetwork, more conv layers
+# more pooling
+# add support to run on gpu(device)
+# upgrade implementation to 2019 paper
+# store compressed states(run through convnetwork)
+# dropout on convnetwork, batchnorm
+# stacked states(to capture movement)?
+# seed?
+# train longer? 1m-10m frames aka 1k-10k episodes?
+# reduce training steps?
+
+# v1 canonical (pc1)
+# episode 99 frames 600 rewards 0.00 mean score(100ep) 0.25
+
+# v2 canonical, 400 episodes (pc2)
+#env crashed
+#episode 152 frames 35 rewards 0.00 mean score(100ep) 0.21
+
+# v3 canonical, 300 episodes, lr 3·10^−4 (pc1)
+# still not learning, could be suffering from sparse rewards, will validate algorithm on different env
+
+# v4 canonical, 400 episodes, lr 3·10^−4, batch size 256 (pc2)
+
+# v5 canonical, 400 episodes, lr 3·10^−4, reward scaling * 10 (pc1)
+
+# v6 fc layers in convnetwork, lr, reward scaling * 10 (pc1)
+

sac.py
@@ -17,13 +17,14 @@ class SoftActorCriticAgent():
         self.critic_q_1 = ActionValueNetwork()
         self.critic_q_2 = ActionValueNetwork()
         self.actor = PolicyNetwork()
-        self.actor_optim = optim.Adam(self.actor.parameters())
-        self.v_optim = optim.Adam(self.critic_v.parameters())
-        self.q1_optim = optim.Adam(self.critic_q_1.parameters())
-        self.q2_optim = optim.Adam(self.critic_q_2.parameters())        
+        self.actor_optim = optim.Adam(self.actor.parameters(), lr=3*10e-4) #0.003
+        self.v_optim = optim.Adam(self.critic_v.parameters(), lr=0.003)
+        self.q1_optim = optim.Adam(self.critic_q_1.parameters(), lr=0.003)
+        self.q2_optim = optim.Adam(self.critic_q_2.parameters(), lr=0.003)        
         self.gamma = 0.99
         self.tau = 0.005
         self.batch_size = 16 #256
+        self.reward_scale = 10
         self.replay_buffer = ReplayBuffer(self.batch_size)
         self.update_target(1)
 
@@ -70,7 +71,7 @@ class SoftActorCriticAgent():
         policy_actions = torch.tanh(z)
 
         # r(st,at) +γEst+1∼p[V ̄ψ(st+1)],
-        target_q = rewards + (self.gamma * self.critic_v_target(next_states) * (1-dones)) 
+        target_q = rewards * self.reward_scale + (self.gamma * self.critic_v_target(next_states) * (1-dones)) 
         q1_loss = F.mse_loss(current_q_1, target_q.detach()) 
         q2_loss = F.mse_loss(current_q_2, target_q.detach())
         self.q1_optim.zero_grad()